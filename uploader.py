78d3f3ced8c182884d2a5036551170402f9c8998
David Dale <ddale@yandex-team.ru>
2019-03-16 21:29:55 +0300

    the plan

---
+++ uploader.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    # todo: read the key from env
+    # todo: read the column map and filenames from config.json
+    # todo: setup the google sheet api
+    # todo: connect to the sheet
+    # todo: parse the sheet
+    # todo: optionally, validate the sheet and print the warnings (especially in the web mode)
+    # todo: convert the data into json format
+    # todo: dump the data
+    pass
