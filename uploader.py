13ea96e46c7de75f6141dc7911ce2e5a2330c8b9
David Dale <ddale@yandex-team.ru>
2019-03-17 22:42:25 +0300

    template for multiple versions and synchronous updater

--- uploader.py
+++ uploader.py
@@ -1,6 +1,35 @@
 import json
+import time
 import os
 from read_sheet import get_creds, get_rows, rows_to_json, update_fields_with_default
+
+
+STATE_CONFIG_FILENAME = 'state_config.json'
+SECONDS_BETWEEN_REFRESH = 60
+
+
+def get_last_updated_time():
+    if os.path.exists(STATE_CONFIG_FILENAME):
+        with open(STATE_CONFIG_FILENAME, 'r') as f:
+            state_config = json.load(f)
+            return state_config['last_updated']
+    return 0
+
+
+def set_last_updated_time(timestamp):
+    with open(STATE_CONFIG_FILENAME, 'w') as f:
+        json.dump({'last_updated': timestamp}, f)
+
+
+def update_if_needed(force_update=False, *args, **kwargs):
+    past = get_last_updated_time()
+    present = time.time()
+    if present - past > SECONDS_BETWEEN_REFRESH or force_update:
+        message = update_people(*args, **kwargs)
+        set_last_updated_time(present)
+    else:
+        message = 'No update is needed as only {} seconds passed before the last one'.format(present - past)
+    return message
 
 
 def update_people(people_filename=None):
@@ -22,4 +51,5 @@ def update_people(people_filename=None):
 
 
 if __name__ == '__main__':
-    update_people(people_filename='static/data.js')
+    # todo: make a background worker script to update the data asynchronously
+    update_people()
--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, render_template, abort
-from uploader import update_people
+from uploader import update_people, update_if_needed
 
 app = Flask(__name__)
 
@@ -10,7 +10,10 @@ with open('history_config.json', 'r', encoding='utf-8') as f:
 
 @app.route('/')
 def home():
-    return render_template('peoplebook.html', period=history_config['current'])
+    update_if_needed()
+    return render_template(
+        'peoplebook.html', period=history_config['current'], period_text=history_config['current_text']
+    )
 
 
 @app.route('/updater')
@@ -22,5 +25,6 @@ def updater():
 @app.route('/history/<period>')
 def history(period):
     if period in history_config['history']:
-        return render_template('peoplebook.html', period=period)
+        update_if_needed()
+        return render_template('peoplebook.html', period=period, period_text=history_config['history'][period])
     abort(404)
