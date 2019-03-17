bdad65fc159552269e86d852cc5d2dd91d18d36f
David Dale <ddale@yandex-team.ru>
2019-03-17 16:58:16 +0300

    function for converting rows to json

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask
 from uploader import update_people
 
 app = Flask(__name__)
@@ -7,9 +7,7 @@ app.config["CACHE_TYPE"] = "null"
 
 @app.route('/')
 def home():
-    return redirect(url_for('static', filename='index.html'))
-    # return app.send_static_file('index.html')
-    # todo: maybe fix the relative links and really send the static file
+    return app.send_static_file('index.html')
 
 
 @app.route('/updater')
