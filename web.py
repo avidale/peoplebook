6713b674ee5d3ca96433c8f3d1ca28038d90b51b
David Dale <ddale@yandex-team.ru>
2019-03-17 20:09:34 +0300

    parse urls for history

--- web.py
+++ web.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, abort
 from uploader import update_people
 
 app = Flask(__name__)
-app.config["CACHE_TYPE"] = "null"
+
+with open('history_config.json', 'r', encoding='utf-8') as f:
+    history_config = json.load(f)
 
 
 @app.route('/')
 def home():
-    return render_template('peoplebook.html')
+    return render_template('peoplebook.html', period=history_config['current'])
 
 
 @app.route('/updater')
 def updater():
     message = update_people()
     return message
+
+
+@app.route('/history/<period>')
+def history(period):
+    if period in history_config['history']:
+        return render_template('peoplebook.html', period=period)
+    abort(404)
