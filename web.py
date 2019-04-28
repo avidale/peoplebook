93577f88b481036df86a7e7dd2b96791b69c0a78
David Dale <ddale@yandex-team.ru>
2019-04-28 14:58:20 +0300

    mongodb-based pages for event and for person

--- web.py
+++ web.py
@@ -1,4 +1,7 @@
 import json
+import os
+import pymongo
+
 from flask import Flask, render_template, abort
 from uploader import update_people, update_if_needed
 
@@ -6,6 +9,15 @@ app = Flask(__name__)
 
 with open('history_config.json', 'r', encoding='utf-8') as f:
     history_config = json.load(f)
+
+
+MONGO_URL = os.environ.get('MONGODB_URI')
+mongo_client = pymongo.MongoClient(MONGO_URL)
+mongo_db = mongo_client.get_default_database()
+mongo_events = mongo_db.get_collection('events')
+mongo_participations = mongo_db.get_collection('event_participations')
+mongo_peoplebook = mongo_db.get_collection('peoplebook')
+
 
 
 @app.route('/')
@@ -28,3 +40,36 @@ def history(period):
         update_if_needed()
         return render_template('peoplebook.html', period=period, period_text=history_config['history'][period])
     abort(404)
+
+
+@app.route('/event/<event_code>')
+def peoplebook_for_event(event_code):
+    the_event = mongo_events.find_one({'code': event_code})
+    if the_event is None:
+        return 'Такого события не найдено!'
+    raw_profiles = list(mongo_participations.aggregate([
+        {
+            '$lookup': {
+                'from': 'peoplebook',
+                'localField': 'username',
+                'foreignField': 'username',
+                'as': 'profiles'
+            }
+        }, {
+            '$match': {'code': event_code, 'engaged': True}
+        }
+    ]))
+    profiles = [p for rp in raw_profiles for p in rp.get('profiles', [])]
+    return render_template(
+        'backend_peoplebook.html',
+        title=the_event.get('title', 'Пиплбук встречи'),
+        profiles=profiles
+    )
+
+
+@app.route('/person/<username>')
+def peoplebook_for_person(username):
+    the_profile = mongo_peoplebook.find_one({'username': username})
+    if the_profile is None:
+        return 'Такого профиля не найдено!'
+    return render_template('single_person.html', profile=the_profile)
