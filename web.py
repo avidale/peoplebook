4f98e4a01bf8fff2efeb613f467d3417b221fc39
KrasnovAU <krasnovau@eruditor-group.com>
2019-12-16 13:23:43 +0300

    pass_bot_info_forward

--- web.py
+++ web.py
@@ -166,7 +166,7 @@ def login_link():
     user = User(user_id)
     login_user(user, remember=True)
     if request.args.get('next'):
-        return redirect(request.args.get('next'))
+        return redirect('{}?{}'.format(request.args.get('next'), request.args.get('bot_info')))
     else:
         return 'Вход выполнен'
 
