b2778e49ae449f3fd6991c7b5cd460a57b43f865
David Dale <ddale@yandex-team.ru>
2020-03-31 01:29:16 +0300

    fix wakeup

--- wakeup.py
+++ wakeup.py
@@ -2,7 +2,12 @@
 This script is used with Heroku scheduler.
 It intends to call a wakeup function in the main.py (if necessary, restart the main web dyno)
 """
+import os
 import requests
 
+
+BASE_URL = os.environ.get('BASE_URL', 'https://kappa-vedi-bot.herokuapp.com/')
+
+
 if __name__ == '__main__':
-    requests.get("https://kappa-vedi-bot.herokuapp.com/wakeup/")
+    requests.get(os.path.join(BASE_URL, "wakeup/"))
