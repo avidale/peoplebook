b25ec8ca2cac81de911d347724ad3c8b1fca5ab1
David <dale.david@mail.ru>
2020-06-27 15:26:59 +0300

    fix a test

--- peoplebot/scenarios/peoplebook_auth.py
+++ peoplebot/scenarios/peoplebook_auth.py
@@ -7,7 +7,7 @@ from config import PEOPLEBOOK_ROOT
 def make_pb_url(relative_path, user_tg_id):
     if relative_path.startswith('/'):
         relative_path = relative_path[1:]
-    new_id = str(user_tg_id) + os.environ.get('login_salt')
+    new_id = str(user_tg_id) + os.environ.get('login_salt', 'no_free_salt')
     auth_token = hashlib.md5(str(new_id).encode('utf-8')).hexdigest()
     return '{}/login_link?bot_info={}&next={}'.format(
         PEOPLEBOOK_ROOT,
